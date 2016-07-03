f3c24b245671645e67dfafa58fdfd21f03fbc546
Datokrat <pa.rei@gmx.de>
2016-07-04 01:11:17 +0200

    debugger files

---
+++ debug.js
@@ -0,0 +1,6 @@
+//This file is used to start the specs from a node script which is practical for various debuggers.
+
+var gulp = require('gulp');
+var gulpfile = require('./gulpfile')
+
+gulp.start('specs');
