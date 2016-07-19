7e009bf7f29066af7daf0cae341b0100673f3346
mnemonic101 <mnemonic@codemonic.net>
2016-07-19 15:36:46 +0200

    struggling with tsc vs. gulp-sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,36 @@
 var gulp = require('gulp'),
-    jasmine = require('gulp-jasmine'),
-    tslint = require('gulp-tslint');
+  jasmine = require('gulp-jasmine'),
+  tslint = require('gulp-tslint');
+  tslint = require('gulp-tslint'),
+  tsc = require('gulp-typescript'),
+	sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('lint', function() {
+gulp.task('lint', function () {
   return gulp.src([
     'src/**/**.ts',
     'spec/**.ts',
     'typings/**.d.ts'
   ])
-  .pipe(tslint({ }))
-  .pipe(tslint.report('verbose'));
+    .pipe(tslint({}))
+    .pipe(tslint.report('verbose'));
 });
 
-gulp.task('tests', function() {
+var tsProject = tsc.createProject("tsconfig.json");
+gulp.task('build', function () {
+  return gulp.src([
+    './**/**.ts',
+    '!./node_modules/**'
+  ])
+    .pipe(sourcemaps.init())
+    .pipe(tsc(tsProject))
+    .js
+    //.pipe(sourcemaps.write('../maps'))
+    .pipe(sourcemaps.write('.', {debug: true, includeContent: false, sourceRoot: '../../'}))
+    .pipe(gulp.dest('lib'));
+})
+
+//gulp.task('tests', ['build'], function () {
+gulp.task('tests', function () {
   return gulp.src('./lib/spec/*.js')
     .pipe(jasmine({ includeStackTrace: true, verbose: true }));
 });
@@ -20,6 +38,6 @@ gulp.task('tests', function() {
 //alternative name for the 'tests' task
 gulp.task('specs', ['tests']);
 
-gulp.task('server', function() {
+gulp.task('server', function () {
   require('./lib/src/server');
 });
