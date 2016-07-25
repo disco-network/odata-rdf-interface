f359a6ad325fc959822d4964a8a3dc724aebc268
Datokrat <pa.rei@gmx.de>
2016-07-25 22:02:26 +0200

    test the branching behavior of property trees

--- spec/propertytree_spec.ts
+++ spec/propertytree_spec.ts
@@ -1,5 +1,53 @@
+import propertyTree = require("../src/adapter/propertytree");
 import mappings = require("../src/adapter/mappings");
 import schema = require("../src/odata/schema");
+
+describe("Property trees", () => {
+  it("should avoid duplicate branches", () => {
+    let TestBranch = class extends propertyTree.Tree {
+      public hash() {
+        return "my hash";
+      }
+      public countBranches() {
+        return Object.keys(this.branches).length;
+      }
+    };
+    let tree = new TestBranch();
+    let branch = new TestBranch();
+    tree.branch(branch);
+    tree.branch(branch);
+
+    expect(tree.countBranches()).toBe(1);
+  });
+
+  it("should avoid duplicate branches after copying", () => {
+    let TestBranch = class extends propertyTree.Tree {
+      constructor(private hashStr: string) {
+        super();
+      }
+      public hash() {
+        return this.hashStr;
+      }
+      public countBranches() {
+        return Object.keys(this.branches).length;
+      }
+    };
+    let tree1 = new TestBranch("1");
+    let tree2 = new TestBranch("2");
+    let branch1a = new TestBranch("a");
+    let branch2a = new TestBranch("a");
+    let branch1a1 = new TestBranch("a/1");
+    let branch2a1 = new TestBranch("a/1");
+    branch1a.branch(branch1a1);
+    tree1.branch(branch1a);
+    branch2a.branch(branch2a1);
+    tree2.branch(branch2a);
+
+    tree2.copyTo(tree1);
+
+    expect(branch1a.countBranches()).toBe(1);
+  });
+});
 
 class TestMapping extends mappings.Mapping {
   public properties: TestPropertyMapping;
