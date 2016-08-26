6b6af3a284e3c91ba04bb2804fa6b600ff1e6149
Datokrat <pa.rei@gmx.de>
2016-08-26 20:12:47 +0200

    documentation of the filterexpressions module

--- src/odata/filterexpressions.ts
+++ src/odata/filterexpressions.ts
@@ -1,5 +1,40 @@
 import * as contract from "../contract";
 
+/*
+ * IValueHandlers should be able to restrict the subtypes of IValues to handle.
+ * This should be enforced by the type checker.
+ * To ensure type checking, it seems that we need lots of recursive generic interface definitions
+ * which turn out to have similarities with the so-called Curiously Recurring Template Pattern.
+ * 
+ * In order to use this module we first need to recursively define a helper object, for example:
+ *
+ *  type AllowedValues = IOrExpression<MyValueWrapper> | IStringLiteral<MyValueWrapper>;
+ *  class MyValueWrapper implements IWrapValue<MyValueWrapper> {
+ *    constructor(private value: AllowedValues) {}
+ *    public getValue(): AllowedValues {
+ *      return this.value;
+ *    }
+ *  }
+ *
+ * Then the type of our ValueHandler will look like this.
+ *
+ *  type MyValueHandler = IValueHandler<MyValueWrapper, [return type]>;
+ *
+ * Or we could do this:
+ *
+ *  class MyValueHandler implements IValueHandler<MyValueWrapper, boolean> {
+ *    /** return true if all values are of type IOrExpression * /
+ *    handle(wrapper: MyValueWrapper): boolean {
+ *      let value = wrapper.getValue();
+ *      if (IOrExpression.is(value)) {
+ *        // Due to TypeScript type guard magic, we can access IOrExpression-specific members.
+ *        return this.handle(this.getLhs()) && this.handle(this.getRhs());
+ *      }
+ *      else return false;
+ *    }
+ *  }
+ */
+
 export interface IValueHandler<TWrapValue extends IWrapValue<TWrapValue>, Return> {
   handle(value: TWrapValue): Return;
 }
